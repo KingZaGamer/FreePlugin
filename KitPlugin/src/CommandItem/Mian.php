29dbb69e188725b5a49f6fa8333dfcbd8faefbfd
G2P <guyza01.2547@gmail.com>
2017-05-06 14:20:27 +0700

    Update Mian.php

--- KitPlugin/src/CommandItem/Mian.php
+++ KitPlugin/src/CommandItem/Mian.php
@@ -15,14 +15,18 @@ class Main extends PluginBase implements Listener//สำคัญ
   {
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
-  
+  //CommandSender คือ คนที่ใช้คำ้ง
+  //Command กะคือคำสั้ง
   public function onCommand(CommandSender $sender, Command $command, $label, array $args)
   {
     switch($cmd->getName()) {
       case "item":
        if ($sender instanceof Player) {//แทนค่า $sender ด้วย Player
         $sender->getInventory()->addItem(Item::get(276, 0, 1));//ให้ของในตัว
-        $sender->sendMessage("You has get Item");
+        $sender->sendMessage("You has get Item");//ส่งข้อความไปหาคนใช้ command
+       } else {//ถ้า sender ไม่ใช้ Player
+         $sender->sendMessgae("Use command in Game!");
+       }
     }
   }
   
