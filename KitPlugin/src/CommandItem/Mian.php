2e4f954b684c3f21c07c14d18bacebf7763dabb6
G2P <guyza01.2547@gmail.com>
2017-05-06 14:18:13 +0700

    Create Mian.php

---
+++ KitPlugin/src/CommandItem/Mian.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kit;//ระบุตำแหน่งของ plugin ไม่จำเปนต้องมี src
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Player;
+use pocketmine\item\Item;
+
+class Main extends PluginBase implements Listener//สำคัญ
+{
+  public function onEnable()//สำคัญ(บอกให้ Server รู้ว่าถ้า plugin ทำงานจะให้ทำอะไร)
+  {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onCommand(CommandSender $sender, Command $command, $label, array $args)
+  {
+    switch($cmd->getName()) {
+      case "item":
+       if ($sender instanceof Player) {//แทนค่า $sender ด้วย Player
+        $sender->getInventory()->addItem(Item::get(276, 0, 1));//ให้ของในตัว
+        $sender->sendMessage("You has get Item");
+    }
+  }
+  
+  public function onDisable()
+  {
+  }
+}
